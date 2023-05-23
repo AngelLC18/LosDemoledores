3137323dde9a016396aa9983a27c2c12d551e5d0
saraagomezz <102100641+saraagomezz@users.noreply.github.com>
2023-05-23 18:40:51 -0300

    creando archivo Formulario de Registro

---
+++ accessim/src/components/FormRegistro.js
@@ -0,0 +1,19 @@
+import React from "react";
+import InputString from "./InputString";
+import Select from "./Select";
+import Checklist from "./Checklist";
+import Textarea from "./MasInfo";
+
+function FormRegistro() {
+    return (
+      <div>
+        <h1>holaaa</h1>
+        <InputString />
+        <Select />
+        <Checklist />
+        <Textarea />
+      </div>  
+    );
+  }
+  
+  export default FormRegistro;
