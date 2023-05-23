86c20b35a21376fad7eb6d4fc00db8b9e692c377
saraagomezz <102100641+saraagomezz@users.noreply.github.com>
2023-05-23 18:46:41 -0300

    renombrando InputText por MasInfo

--- accessim/src/components/MasInfo.js
+++ accessim/src/components/MasInfo.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-function Textarea(){
+function MasInfo(){
     return(
         <textarea placeholder="Mas Info"></textarea>
     );
 }
-export default Textarea;
+export default MasInfo;
