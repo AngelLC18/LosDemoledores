ebb9a9ea5890b68d95e016aa1e5ef8d1d7d93f37
saraagomezz <102100641+saraagomezz@users.noreply.github.com>
2023-05-23 18:37:44 -0300

    modificando App.js

--- accessim/src/App.js
+++ accessim/src/App.js
@@ -1,19 +1,10 @@
 import React from "react";
-import InputString from "./components/InputString";
-import Select from "./components/Select";
-import Checklist from "./components/Checklist";
-import Textarea from "./components/InputText";
+import FormRegistro from "./components/FormRegistro";
 //import './App.css';
 
 function App() {
-  
   return (
-    <React.Fragment>{/*esta etiqueta es invisible*/}
-      <InputString />
-      <Select />
-      <Checklist />
-      <Textarea />
-    </React.Fragment>  
+    <FormRegistro />
   );
 }
 
