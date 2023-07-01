da02dcc7d449356331fe5d4c97e5afa63bd4cf55
saraagomezz <102100641+saraagomezz@users.noreply.github.com>
2023-07-01 11:52:27 -0300

    Creacndo componente Footer

---
+++ accessim/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer (){
+    return(
+        <footer className="flex justify-bottom footer footer-bottom p-10 bg-primary text-primary-content ">
+            
+        </footer>
+    )
+}
+export default Footer; 
