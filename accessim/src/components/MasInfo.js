597c14d740e20aa2411fe055487150c0f243dbae
saraagomezz <102100641+saraagomezz@users.noreply.github.com>
2023-07-01 10:54:12 -0300

    Creando funcion componente MasInfo (textarea)

---
+++ accessim/src/components/MasInfo.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function MasInfo(props){
+    return(
+        <textarea className="textarea textarea-info" placeholder={props.placeholder}></textarea>
+    );
+}
+export default MasInfo;
