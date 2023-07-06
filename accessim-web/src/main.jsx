eebf73cec9a44ad00ad3cd5357a19bfb5b8e5607
Angel Luciano Ciancio <ciancioangel.epet20@gmail.com>
2023-07-06 17:55:36 -0300

    Agregando el context al main.jsx

--- accessim-web/src/main.jsx
+++ accessim-web/src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { ThemeProvider } from "./context/Theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 );
