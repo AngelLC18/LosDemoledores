4b81a6e053b386be20db3950b32da8f35c0a54e7
saraagomezz <102100641+saraagomezz@users.noreply.github.com>
2023-07-27 18:27:14 -0300

    Agrgando componente Footer a App.js

--- accessim/src/App.js
+++ accessim/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import React from 'react';
 import FormRegistro from './components/FormRegistro';
+import Footer from './components/Footer';
 
 
 function App(){
     return (
-        <div className="flex justify-center items-center w-full h-screen bg-gradient-to-tr from-blue-500 to-white-200 ">
-            <FormRegistro />
+        <div>
+            <div className="flex justify-center items-center w-full h-screen bg-gradient-to-tr from-blue-500 to-white-200 ">
+                <FormRegistro />
+            </div>
+            <div className="footer">
+                <Footer />
+            </div>
         </div>
     );
 } export default App;   
