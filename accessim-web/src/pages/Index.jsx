b37b630998bdede838c3f94384efb00269eaa98a
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-08-09 15:30:43 -0300

    Añadiendo login

--- accessim-web/src/pages/Index.jsx
+++ accessim-web/src/pages/Index.jsx
@@ -1,13 +1,18 @@
-import React from "react";
 import Navbar from "../components/navbar/NavBar";
+import Sidebar from "../components/sidebar/Sidebar";
+import Modals from "../components/modal/Modals";
+import Footer from "../components/footer/Footer";
 
 const Index = () => {
   return (
     <div className=" w-full min-h-screen grid grid-rows-3">
       <Navbar />
-      <main className="row-span-2 flex flex-col justify-center min-h-screen items-center">
-        <h1 className="text-6xl font-bold font-mono">Bienvenido</h1>
+      <Sidebar />
+      <main className="row-span-3 flex flex-col bg-sky-300 dark:bg-slate-900 justify-center h-screen items-center">
+        <h1 className="text-6xl font-bold font-mono">AccesSim</h1>
+        <Modals />
       </main>
+      <Footer />
     </div>
   );
 };
