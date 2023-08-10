31e5c21ebbf41f3b7cb46461f9d98949d2f4fb06
gian <103594680+Gian-scovasso@users.noreply.github.com>
2023-08-10 18:49:30 -0300

    caja

--- accessim/src/components/perfil.jsx
+++ accessim/src/components/perfil.jsx
@@ -3,3 +3,7 @@ import React from 'react';
 const Perfil = ({ nombre, edad, ubicacion, foto }) => {
     return (
         <div class="profile-box">
+            <div class="profile">
+      <img src="dado.jpeg" alt="foto Perfil"/>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec elit nisi. Mauris ac dolor sit amet lectus ullamcorper tristique.</p>
+    </div>
