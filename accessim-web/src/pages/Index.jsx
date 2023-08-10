61b2084ea32c0e4e5b977eee66126ad5e26f141e
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-08-10 15:17:18 -0300

    Mejorando login

--- accessim-web/src/pages/Index.jsx
+++ accessim-web/src/pages/Index.jsx
@@ -5,11 +5,11 @@ import Footer from "../components/footer/Footer";
 
 const Index = () => {
   return (
-    <div className=" w-full min-h-screen grid grid-rows-3 dark:bg-slate-950 bg-sky-300">
+    <div className=" w-full min-h-screen grid grid-rows-3 dark:bg-slate-950 bg-orange-100">
       <Navbar />
       <main className="row-span-2 flex flex-col justify-center min-h-screen items-center">
-        <div className="shadow-md rounded-xl dark:bg-slate-700 bg-white h-[400px] w-[520px] mt-4">
-          <form className="  bg-gray-100 dark:bg-slate-700 h-[400px]  p-3 drop-shadow-lg rounded-xl  ">
+        <div className="shadow-md rounded-xl dark:bg-slate-700 bg-white h-[400px] md:h-[400px] w-[300px] md:w-[520px] mt-4">
+          <form className="  bg-white dark:bg-slate-700 h-[400px]  p-3 drop-shadow-lg rounded-xl  ">
             <div className="flex flex-row flex-wrap gap-4 p-2 justify-center">
               <div className="w-56">
                 <label className="relative cursor-text ">
@@ -20,7 +20,7 @@ const Index = () => {
                     autoComplete="off"
                     id="usuario"
                   />
-                  <span className="text-opacity-80 dark:text-white   dark:bg-slate-700 text-gray-600 bg-gray-100 absolute left-3 -top-[2px] px-1 transition duration-200 input-text">
+                  <span className="text-opacity-80 dark:text-white   dark:bg-slate-700 text-gray-600 bg-white absolute left-3 -top-[2px] px-1 transition duration-200 input-text">
                     Usuario
                   </span>
                 </label>
@@ -28,13 +28,13 @@ const Index = () => {
               <div className="w-56">
                 <label className="relative cursor-text ">
                   <input
-                    type="password"
+                    type="text"
                     placeholder="Contraseña"
                     className="h-11 w-56 bg-transparent pl-2 dark:text-white   border-black border-[1px] rounded-[4px] border-opacity-30 outline-none focus:border-blue-700 placeholder-gray-300 placeholder-opacity-0 transition duration-200"
                     autoComplete="off"
                     id="contraseña"
                   />
-                  <span className="text-opacity-80 dark:text-white   dark:bg-slate-700 text-gray-600 bg-gray-100 absolute left-3 -top-[2px] px-1 transition duration-200 input-text">
+                  <span className="text-opacity-80 dark:text-white   dark:bg-slate-700 text-gray-600 bg-white absolute left-3 -top-[2px] px-1 transition duration-200 input-text">
                     Contraseña
                   </span>
                 </label>
@@ -48,7 +48,7 @@ const Index = () => {
                     className="h-11 w-56 bg-transparent pl-2 dark:text-white   border-black border-[1px] rounded-[4px] border-opacity-30 outline-none focus:border-blue-700 placeholder-gray-300 placeholder-opacity-0 transition duration-200"
                     autoComplete="off"
                   />
-                  <span className="text-opacity-80 dark:text-white   dark:bg-slate-700 text-gray-600 bg-gray-100 absolute left-3 -top-[2px] px-1 transition duration-200 input-text">
+                  <span className="text-opacity-80 dark:text-white   dark:bg-slate-700 text-gray-600 bg-white absolute left-3 -top-[2px] px-1 transition duration-200 input-text">
                     Nombre
                   </span>
                 </label>
@@ -62,7 +62,7 @@ const Index = () => {
                     className="h-11 w-56 bg-transparent pl-2 dark:text-white   border-black border-[1px] rounded-[4px] border-opacity-30 outline-none focus:border-blue-700 placeholder-gray-300 placeholder-opacity-0 transition duration-200"
                     autoComplete="off"
                   />
-                  <span className="text-opacity-80 dark:text-white   dark:bg-slate-700 text-gray-600 bg-gray-100 absolute left-3 -top-[2px] px-1 transition duration-200 input-text">
+                  <span className="text-opacity-80 dark:text-white   dark:bg-slate-700 text-gray-600 bg-white absolute left-3 -top-[2px] px-1 transition duration-200 input-text">
                     Apellido
                   </span>
                 </label>
@@ -71,12 +71,12 @@ const Index = () => {
                 <label className="relative cursor-text">
                   <input
                     id="email"
-                    type="email"
+                    type="text"
                     placeholder="Email"
                     className="h-11 w-56 bg-transparent pl-2 dark:text-white   border-black border-[1px] rounded-[4px] border-opacity-30 outline-none focus:border-blue-700 placeholder-gray-300 placeholder-opacity-0 transition duration-200"
                     autoComplete="off"
                   />
-                  <span className="text-opacity-80 dark:text-white   dark:bg-slate-700 text-gray-600 bg-gray-100 absolute left-3 -top-[2px] px-1 transition duration-200 input-text">
+                  <span className="text-opacity-80 dark:text-white   dark:bg-slate-700 text-gray-600 bg-white absolute left-3 -top-[2px] px-1 transition duration-200 input-text">
                     Correo Electronico
                   </span>
                 </label>
@@ -105,14 +105,14 @@ const Index = () => {
               <div className="flex flex-col gap-2">
                 <button
                   type="submit"
-                  className="ml-1 bg-white text-black py-2 px-6 rounded-md hover:bg-gray-100"
+                  className="ml-1 bg-red-100 text-black py-2 px-6 rounded-md shadow-lg hover:bg-gray-100"
                 >
                   Iniciar sesión con google
                   <i class="fa-brands fa-google m-2"></i>
                 </button>
                 <button
                   type="submit"
-                  className="ml-1 bg-blue-800 text-white py-2 px-6 rounded-md hover:bg-blue-900"
+                  className="ml-1 bg-blue-800 text-white py-2 px-6 rounded-md shadow-lg hover:bg-blue-900"
                 >
                   Iniciar sesión con facebook
                   <i class="fa-brands fa-facebook m-2"></i>
