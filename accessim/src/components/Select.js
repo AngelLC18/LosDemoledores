c67e3cf50e9e46f36c6d4e73b99f020877801800
saraagomezz <102100641+saraagomezz@users.noreply.github.com>
2023-08-21 17:32:56 -0300

    Reajustando Select

--- accessim/src/components/Select.js
+++ accessim/src/components/Select.js
@@ -1,8 +1,13 @@
 import React from "react";
 
-function Select() {
+function Select(props) {
     return(
-        <select placeholder="Seleccione las accesibilidades que brinda"></select>
+        <select className="select select-info w-full max-w-xs" defaultValue='option'>
+            <option disabled value ='option'>{props.disabledOption}</option>
+            <option value={props.value1}>{props.labelSelect1}</option>
+            <option value={props.value2}>{props.labelSelect2}</option>
+            <option value={props.value3}>{props.labelSelect3}</option>
+        </select>
     );
 }
 
