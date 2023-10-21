9a4554a10161a0b3a439d8189d552f34b582074c
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-10-21 18:12:27 -0300

    Agregando ruta login a la web

--- accessim-web/src/routes/Routes.jsx
+++ accessim-web/src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "../pages/Index";
+import Login from "../pages/login/Login";
 
 const Rutas = () => {
   return (
@@ -8,6 +9,7 @@ const Rutas = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
