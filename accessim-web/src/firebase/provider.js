58c8d879cf509b7d48ac09d3efea6a98557a7393
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-10-24 16:19:35 -0300

    Agregando providers para el inicio de sesión

---
+++ accessim-web/src/firebase/provider.js
@@ -0,0 +1,118 @@
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
+  updateProfile,
+} from "firebase/auth";
+import { FirebaseAuth } from "./config";
+import { getErrorMessageFromFirebaseError } from "../utils/getErrorMessageFromFirebase";
+
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
+export const singInWithGithub = async () => {
+  try {
+    const result = await signInWithPopup(FirebaseAuth, githubProvider);
+    const { displayName, email, photoURL, uid } = result.user;
+
+    return {
+      ok: true,
+      // User info
+      displayName,
+      email,
+      photoURL,
+      uid,
+    };
+  } catch (error) {
+    const errorMessage = getErrorMessageFromFirebaseError(error.message);
+
+    return {
+      ok: false,
+      errorMessage,
+    };
+  }
+};
+export const singInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(FirebaseAuth, googleProvider);
+    // const credentials = GoogleAuthProvider.credentialFromResult( result );
+    const { displayName, email, photoURL, uid } = result.user;
+
+    return {
+      ok: true,
+      // User info
+      displayName,
+      email,
+      photoURL,
+      uid,
+    };
+  } catch (error) {
+    const errorMessage = getErrorMessageFromFirebaseError(error.message);
+
+    return {
+      ok: false,
+      errorMessage,
+    };
+  }
+};
+
+export const registerUserWithEmailPassword = async ({
+  email,
+  password,
+  displayName,
+}) => {
+  try {
+    const resp = await createUserWithEmailAndPassword(
+      FirebaseAuth,
+      email,
+      password
+    );
+    const { uid, photoURL } = resp.user;
+
+    await updateProfile(FirebaseAuth.currentUser, { displayName });
+
+    return {
+      ok: true,
+      uid,
+      photoURL,
+      email,
+      displayName,
+    };
+  } catch (error) {
+    const errorMessage = getErrorMessageFromFirebaseError(error.message);
+    return {
+      ok: false,
+      errorMessage,
+    };
+  }
+};
+
+export const loginWithEmailPassword = async ({ email, password }) => {
+  try {
+    const resp = await signInWithEmailAndPassword(
+      FirebaseAuth,
+      email,
+      password
+    );
+    const { uid, photoURL, displayName } = resp.user;
+
+    return {
+      ok: true,
+      uid,
+      photoURL,
+      displayName,
+    };
+  } catch (error) {
+    const errorMessage = getErrorMessageFromFirebaseError(error.message);
+    return {
+      ok: false,
+      errorMessage,
+    };
+  }
+};
+
+export const logoutFirebase = async () => {
+  return await FirebaseAuth.signOut();
+};
