06c39e7d4f6fca203fb531a19912f7a2439a2533
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-10-24 16:21:30 -0300

    Componente para cargar el loader

---
+++ accessim-web/src/components/ui/CheckingAuth.jsx
@@ -0,0 +1,7 @@
+export const CheckingAuth = () => {
+  return (
+    <div className="flex items-center min-h-screen dark:bg-gray-950 bg-white text-black dark:text-gray-300 flex-col gap-6 justify-center px-6 py-12 lg:px-8">
+      <span className="loader"></span>
+    </div>
+  );
+};
