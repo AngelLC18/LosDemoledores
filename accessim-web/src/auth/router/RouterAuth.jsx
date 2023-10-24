b244592251c280131f54bc119740f81948d5f1b2
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-10-24 16:20:36 -0300

    Rutas para login y register de la web

---
+++ accessim-web/src/auth/router/RouterAuth.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Login, Register } from "../pages"
+
+export const RouterAuth = () => {
+    return (
+        <Routes>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+
+            <Route path='/*' element={<Navigate to="/auth/login" />} />
+        </Routes>
+    )
+}
