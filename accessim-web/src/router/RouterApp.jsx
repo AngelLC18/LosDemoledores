f43ff431318fefa54df67f2f8a779d0741328b50
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-10-24 16:19:05 -0300

    Insertando rutas con validación para la app

---
+++ accessim-web/src/router/RouterApp.jsx
@@ -0,0 +1,25 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { RouterAuth } from "../auth";
+import { RouterFirebase } from "../app/index";
+import { useCheckAuth } from "../hooks";
+import { CheckingAuth } from "../components/ui";
+
+export const RouterApp = () => {
+  const status = useCheckAuth();
+
+  if (status === "checking") {
+    return <CheckingAuth />;
+  }
+
+  return (
+    <Routes>
+      {status === "authenticated" ? (
+        <Route path="/*" element={<RouterFirebase />} />
+      ) : (
+        <Route path="/auth/*" element={<RouterAuth />} />
+      )}
+
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
+    </Routes>
+  );
+};
