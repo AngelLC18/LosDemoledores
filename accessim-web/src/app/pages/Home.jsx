06b55987a17d134f970bc9789fd70356a5579c2f
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-10-24 16:16:17 -0300

    Agregando home del proyecto

---
+++ accessim-web/src/app/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../components/footer/Footer";
+import Accesibility_tools from "../../components/Accessibility_tools";
+import Navbar from "./components/navbar/NavBar";
+export const Home = () => {
+  return (
+    <div className=" w-full min-h-screen grid grid-rows-3 dark:bg-slate-950 bg-orange-100">
+      <Navbar />
+      <main className="row-span-2 flex flex-col justify-center min-h-screen items-center"></main>
+      <Footer />
+      <Accesibility_tools />
+    </div>
+  );
+};
