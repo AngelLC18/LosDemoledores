de675e56ad127b14f9e052cfab8d14a402a8aba3
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-10-24 16:21:59 -0300

    Componente de titulo para la web

---
+++ accessim-web/src/components/ui/Title.jsx
@@ -0,0 +1,7 @@
+export const Title = ({ children }) => {
+  return (
+    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-800 dark:text-white">
+      {children}
+    </h2>
+  );
+};
