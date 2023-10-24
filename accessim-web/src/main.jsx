da667b993f45ef38b48a340e2763989699200696
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-10-24 16:14:49 -0300

    Modificando main.jsx

--- accessim-web/src/main.jsx
+++ accessim-web/src/main.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "./context/Theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ThemeProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ThemeProvider>
+  <React.StrictMode>
+    <ThemeProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ThemeProvider>
+  </React.StrictMode>
 );
