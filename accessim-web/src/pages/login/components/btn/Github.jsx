c7e45516b8fd33ef38ad6f476787f9bf0cb78f0b
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-10-24 16:14:41 -0300

    Borrando archivos obsoletos

--- accessim-web/src/pages/login/components/btn/Github.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Github = () => {
-  return <div>Github</div>;
-};
-
-export default Github;
--- accessim-web/src/pages/login/components/btn/Google.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Google = () => {
-  return <div>Google</div>;
-};
-
-export default Google;
