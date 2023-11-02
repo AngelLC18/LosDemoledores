747a576f2d9e7dd308fac82150adecf30c74b1de
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-11-02 17:22:50 -0300

    Editando layout del login

--- accessim-web/src/auth/pages/Layout.jsx
+++ accessim-web/src/auth/pages/Layout.jsx
@@ -3,16 +3,17 @@ import { Title } from "../../components/ui/Title";
 
 export const Layout = ({ title, children }) => {
   return (
-    <div className="flex min-h-screen bg-[url('/src/assets/DreamShaper_v7_cities_and_accesiblity_0.jpg')] bg-cover  dark:bg-slate-900 text-gray-300 flex-col gap-6 justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm opacity-100">
+    <div className="flex min-h-screen bg-[url('/src/assets/DreamShaper_v7_cities_and_accesiblity_0.jpg')] bg-cover text-gray-300 flex-col gap-6 justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm opacity-100 ">
         <img
-          className="mx-auto h-12 w-auto "
-          src="/src/assets/png_ejemplo.svg"
+          className="mx-auto h-32 w-auto fill-white "
+          src="/src/assets/accessim_logo_white.png"
           alt="Accesim"
         />
+
         <Title>{title}</Title>
       </div>
-      <div className="flex flex-col gap-4 sm:mx-auto sm:w-full sm:max-w-sm">
+      <div className="flex flex-col gap-4 sm:mx-auto sm:w-full sm:max-w-sm bg-white p-4 dark:bg-slate-800 rounded-lg">
         {children}
       </div>
       <Accesibility_tools />
