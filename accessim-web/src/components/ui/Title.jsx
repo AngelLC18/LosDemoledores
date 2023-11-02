1753ee57125dedfd52c9b0ce4eeb6bf49c71114d
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-11-02 17:23:15 -0300

    Editando css del componente tittle

--- accessim-web/src/components/ui/Title.jsx
+++ accessim-web/src/components/ui/Title.jsx
@@ -1,6 +1,6 @@
 export const Title = ({ children }) => {
   return (
-    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-800 dark:text-white">
+    <h2 className="mt-10 text-center text-2xl font-semibold leading-9 tracking-tight  text-black dark:text-white rounded-3xl p-3 bg-white dark:bg-slate-800 bg-opacity-80">
       {children}
     </h2>
   );
