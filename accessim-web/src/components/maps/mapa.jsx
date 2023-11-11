0e9a6175e12f532e24338493aacf7bbe69703fc3
gian <103594680+Gian-scovasso@users.noreply.github.com>
2023-11-11 15:57:03 -0300

    contenido de maps

---
+++ accessim-web/src/components/maps/mapa.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Mapa from './maps';
+
+const App = () => {
+  const ubicacion = { lat: 37.7749, lng: -122.4194 }; 
+
+  return (
+    <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={{ flex: 1, border: '1px solid #ccc', padding: '20px' }}>
+        <h1>Contenido en el Lado Izquierdo</h1>
+        
+      </div>
+
+      <div style={{ flex: 1, position: 'relative' }}>
+        <Mapa ubicacion={ubicacion} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
