46c3d788d419fa71d13d8cc203c2cee0390d8665
ZahiraF <fernandez.zahira.epet20@gmail.com>
2023-11-11 17:45:47 -0300

    agregar estilos a textos de home

--- accessim-web/src/pages/home/Home.jsx
+++ accessim-web/src/pages/home/Home.jsx
@@ -9,16 +9,16 @@ const Home = () => {
         <div className="home-container">
             <Navbar />
             <div className="home-text-section">
-                <h1 className="home-title">Bienvenido a Accessim</h1>
-                <p className="home-paragraph">Aqui encontraras información sobre la accesibilidad en los locales de tu ciudad.</p>
-                <h1>Busca y localiza puntos de interés accesibles</h1>
-                <p>Accessim te permite localizar establecimientos que cumplan con los requisitos que buscas. Encontrarás cualquier establecimiento adaptado a tus necesidades. Las personas con discapacidad (motora, auditiva, visual) tienen necesidades especiales, pero no son los unicos e necesitar información sobre accesibilidad.</p>
-                <p>Hay otras personas con otras necesidades, como vegetarianos o veganos, personas que tengan mascotas o bebés, personas con alergia alimenticia, o cualquier otra barrera que impida que accedan a lo mismo que otra persona a su lado.</p>
-                <h1>Accedé de manera rápida y sencilla</h1>
-                <p>Registrate</p>
-                <p>Filtra las categorías que buscas y necesidades que tenes</p>
-                <h1>¿Quieres que tu empresa aparezca en nuestro mapa?</h1>
-                <p>Unite</p>
+                <h1 className="home-title" class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight" >Bienvenido a Accessim</h1>
+                <p className="home-paragraph" class="mt-6 text-xl leading-8 text-gray-700">Aqui encontraras información sobre la accesibilidad en los locales de tu ciudad.</p>
+                <h1 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Busca y localiza puntos de interés accesibles</h1>
+                <p class="mt-6 text-xl leading-8 text-gray-700">Accessim te permite localizar establecimientos que cumplan con los requisitos que buscas. Encontrarás cualquier establecimiento adaptado a tus necesidades. Las personas con discapacidad (motora, auditiva, visual) tienen necesidades especiales, pero no son los unicos e necesitar información sobre accesibilidad.</p>
+                <p class="mt-6 text-xl leading-8 text-gray-700">Hay otras personas con otras necesidades, como vegetarianos o veganos, personas que tengan mascotas o bebés, personas con alergia alimenticia, o cualquier otra barrera que impida que accedan a lo mismo que otra persona a su lado.</p>
+                <h1 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Accedé de manera rápida y sencilla</h1>
+                <p class="mt-6 text-xl leading-8 text-gray-700">Registrate</p>
+                <p class="mt-6 text-xl leading-8 text-gray-700">Filtra las categorías que buscas y necesidades que tenes</p>
+                <h1 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">¿Quieres que tu empresa aparezca en nuestro mapa?</h1>
+                <p class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6">Unite</p>
             </div>
         </div>
     );
