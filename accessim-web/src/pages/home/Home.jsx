1e5ddecb7b338f041a2a3e7680bcb519c599d530
ZahiraF <fernandez.zahira.epet20@gmail.com>
2023-11-12 13:50:19 -0300

    cambiar texto de boton

--- accessim-web/src/pages/home/Home.jsx
+++ accessim-web/src/pages/home/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
                         <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">¿Queres que tu empresa aparezca en nuestro mapa?</h2>
                     </a>
                     <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Registrate
+                        Registra tu local
                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                         </svg>
