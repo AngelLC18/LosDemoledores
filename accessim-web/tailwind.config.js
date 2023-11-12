2147eaf151d76d0d8e9088f0a9595f86c41e4be3
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-11-12 11:54:57 -0300

    Borrancho regla innecesaria

--- accessim-web/tailwind.config.js
+++ accessim-web/tailwind.config.js
@@ -5,8 +5,6 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-
-    // Path to the Tremor module
     "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
