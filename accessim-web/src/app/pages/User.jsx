67d98a2ada55989cc5bbb3fa368f54aea80c57e2
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-11-13 23:38:26 -0300

    Actualizando diseño de la pantalla de usuario

--- accessim-web/src/app/pages/User.jsx
+++ accessim-web/src/app/pages/User.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
 import "./user.css";
+import Navbar from "./components/navbar/NavBar";
+import Footer from "../../components/footer/Footer";
+import Accesibility_tools from "../../components/Accessibility_tools";
 export const User = () => {
   const {
     startUpdatingUser,
@@ -28,73 +31,78 @@ export const User = () => {
     }
   };
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-slate-500">
-      <h1 className="mb-4 text-2xl font-bold text-gray-700">User Profile</h1>
-      <form
-        onSubmit={handleSubmit}
-        className="p-6 mt-4 bg-white rounded shadow-md w-80 text-black"
-      >
-        <div className="form-group">
-          <label className="text-gray-700">Current Email:</label>
-          <input
-            type="email"
-            value={currentEmail}
-            readOnly
-            className="form-control"
-          />
-        </div>
-        <div className="form-group mt-4">
-          <label className="text-gray-700">New Email:</label>
-          <input
-            type="email"
-            value={newEmail}
-            onChange={(e) => setNewEmail(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        <div className="form-group mt-4">
-          <label className="text-gray-700">Current Password:</label>
-          <input
-            type="password"
-            value={currentPassword}
-            onChange={(e) => setCurrentPassword(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        <div className="form-group mt-4">
-          <label className="text-gray-700">New Password:</label>
-          <input
-            type="password"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        <div className="form-group mt-4">
-          <label className="text-gray-700">Current Username:</label>
-          <input
-            type="text"
-            value={currentDisplayName}
-            readOnly
-            className="form-control"
-          />
-        </div>
-        <div className="form-group mt-4">
-          <label className="text-gray-700">New Username:</label>
-          <input
-            type="text"
-            value={newDisplayName}
-            onChange={(e) => setNewDisplayName(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full mt-6 px-4 py-2 text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700"
+    <div className="w-full min-h-screen grid grid-rows-3 dark:bg-slate-950 bg-orange-100">
+      <Navbar />
+      <main className="row-span-2 flex flex-col min-h-screen justify-center items-center">
+        <h1 className="mb-4 text-2xl font-bold text-gray-700">Perfil</h1>
+        <form
+          onSubmit={handleSubmit}
+          className="p-6 mt-4 bg-white w-[85%] rounded shadow-md grid grid-cols-2 gap-4 text-black"
         >
-          Update Profile
-        </button>
-      </form>
+          <div className="form-group">
+            <label className="text-gray-700">Correo actual:</label>
+            <input
+              type="email"
+              value={currentEmail}
+              readOnly
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label className="text-gray-700">Nuevo correo:</label>
+            <input
+              type="email"
+              value={newEmail}
+              onChange={(e) => setNewEmail(e.target.value)}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label className="text-gray-700">Contraseña actual:</label>
+            <input
+              type="password"
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label className="text-gray-700">Nueva contraseña:</label>
+            <input
+              type="password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label className="text-gray-700">Nombre de usuario actual:</label>
+            <input
+              type="text"
+              value={currentDisplayName}
+              readOnly
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label className="text-gray-700">Nuevo nombre de usuario:</label>
+            <input
+              type="text"
+              value={newDisplayName}
+              onChange={(e) => setNewDisplayName(e.target.value)}
+              className="form-control"
+            />
+          </div>
+          <button
+            type="submit"
+            className="col-span-2 mt-6 px-4 py-2 text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700"
+          >
+            Update Profile
+          </button>
+        </form>
+      </main>
+      <Footer />
+      <Accesibility_tools />
     </div>
   );
 };
