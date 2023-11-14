a0fb23735dd12d5ba029cf91baa1521ad3814507
saraagomezz <102100641+saraagomezz@users.noreply.github.com>
2023-11-14 18:40:28 -0300

    Redimensionando componente Footer

--- accessim-web/src/components/footer/Footer.jsx
+++ accessim-web/src/components/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+/*import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -8,6 +8,38 @@ const Footer = () => {
     >
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2023{" "}
+          <a
+            href="https://github.com/AngelLC18/ProyectoMuniNQN"
+            className="hover:underline"
+          >
+            AccesSim™
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+*/
+
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer
+      className="bg-white shadow dark:bg-slate-950 w-full"
+      style={{
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+      }}
+    >
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        {/* <hr className=" border-gray-200 sm:mx-auto dark:border-gray-700" />*/}
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
           <a
