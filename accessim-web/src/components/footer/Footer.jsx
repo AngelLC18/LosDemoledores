1c457cad7e25d3663e44d2250ec10a4af69df38d
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-11-13 23:37:57 -0300

    Cambiando diseño del footer

--- accessim-web/src/components/footer/Footer.jsx
+++ accessim-web/src/components/footer/Footer.jsx
@@ -2,11 +2,8 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer
-      className="bg-white 
-           shadow dark:bg-slate-950 w-full"
-    >
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+    <footer className="bg-white  dark:bg-gray-950 border-t border-gray-200 dark:border-gray-700">
+      <div className="w-full  mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
