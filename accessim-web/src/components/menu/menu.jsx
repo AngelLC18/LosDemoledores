679eb1f6c66e76da62390659b75293263e163831
gian <103594680+Gian-scovasso@users.noreply.github.com>
2023-11-14 13:10:01 -0300

    categorias

--- accessim-web/src/components/menu/menu.jsx
+++ accessim-web/src/components/menu/menu.jsx
@@ -1,3 +1,13 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Dropdown, DropdownButton, DropdownItem } from "reactstrap";
+
+const App = () => {
+    const [categorias, setCategorias] = useState([
+      "Celiacos",
+      "Diabeticos",
+      "Hipertensos",
+      "Veganos",
+      "Vegetarianos",
+    ]);
+    
