1f755ebfb3b23b047adda65c49213e9ea7728ed1
saraagomezz <102100641+saraagomezz@users.noreply.github.com>
2023-11-14 13:50:45 -0300

    Restableciendo configuracion de firebase

--- accessim-web/src/services/firebaseConfig.js
+++ accessim-web/src/services/firebaseConfig.js
@@ -1,3 +1,4 @@
+/*
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -22,3 +23,28 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
+*/
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-YfDpm82B0KskzwZfg5winD5lzWLi628",
+  authDomain: "accessim-2ae77.firebaseapp.com",
+  databaseURL: "https://accessim-2ae77-default-rtdb.firebaseio.com",
+  projectId: "accessim-2ae77",
+  storageBucket: "accessim-2ae77.appspot.com",
+  messagingSenderId: "449927592576",
+  appId: "1:449927592576:web:c1cbbdde66aa266c350465",
+  measurementId: "G-MGW98NRRKE",
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth(FirebaseApp);
+export const FirebaseDB = getFirestore(FirebaseApp);
