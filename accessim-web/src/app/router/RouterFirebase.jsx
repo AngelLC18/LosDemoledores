98bfa7db5b74c7ae14f4bd550b989ff082ca4113
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-11-14 17:16:47 -0300

    Agrendo ruta de prueba para el formulario

--- accessim-web/src/app/router/RouterFirebase.jsx
+++ accessim-web/src/app/router/RouterFirebase.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Home } from "../pages";
 import { User } from "../pages";
+import Form_rubro from "../pages/Form_rubro";
 
 export const RouterFirebase = () => {
   return (
@@ -8,6 +9,7 @@ export const RouterFirebase = () => {
       <Route path="/" element={<Home />} />
       <Route path="/user/:displayname" element={<User />} />
       <Route path="/*" element={<Navigate to="/" />} />
+      <Route path="/formulario" element={<Form_rubro />} />
     </Routes>
   );
 };
