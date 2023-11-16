ff440cad26721383b3e64ace61f718feb91486cd
gian <103594680+Gian-scovasso@users.noreply.github.com>
2023-11-16 17:05:53 -0300

    cambio e instalaciones

--- accessim-web/src/components/maps/maps.jsx
+++ accessim-web/src/components/maps/maps.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-const Mapa = ({ ubicacion }) => {
-  const mapStyles = {
-    height: '100%',
-    width: '100%',
-  };
-
+const MapContainer = () => {
+  const mapStyles = {        
+    height: "100vh",
+    width: "100%"};
+  
+  const defaultCenter = {
+    lat: 41.3851, lng: 2.1734
+  }
+  
   return (
-    <LoadScript
-      googleMapsApiKey="falta api"
-    >
-      <GoogleMap
-        mapContainerStyle={mapStyles}
-        zoom={13}
-        center={ubicacion}
-      >
-        <Marker position={ubicacion} />
-      </GoogleMap>
-    </LoadScript>
-  );
-};
+     <LoadScript
+       googleMapsApiKey='AIzaSyAWyMMdLdBrlKRUJUuEjG_CMm-E9KoKdVE'>
+        <GoogleMap
+          mapContainerStyle={mapStyles}
+          zoom={13}
+          center={defaultCenter}
+        />
+     </LoadScript>
+  )
+}
 
-export default Mapa;
+export default MapContainer;
